f32555fb71d734e7c2ccbd4490923dffc9cc0e86
sambage29-max <sambage29@gmail.com>
2026-07-28 11:12:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,3 +17,26 @@ st.success("Step 1 Successful ✅")
 
 st.write("Token loaded successfully ✅")
 st.write(ACCESS_TOKEN[:10] + "...")
+url = "https://api.upstox.com/v2/market-quote/ltp"
+
+params = {
+    "instrument_key": "NSE_INDEX|Nifty 50"
+}
+
+headers = {
+    "Accept": "application/json",
+    "Authorization": f"Bearer {ACCESS_TOKEN}"
+}
+
+response = requests.get(
+    url,
+    headers=headers,
+    params=params
+)
+
+st.write("Status Code:", response.status_code)
+
+try:
+    st.json(response.json())
+except Exception:
+    st.write(response.text)
