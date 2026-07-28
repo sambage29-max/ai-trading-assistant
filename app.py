0c87b79b604272216e58991730778fb79df662a1
sambage29-max <sambage29@gmail.com>
2026-07-28 11:05:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import streamlit as st
 import requests
+ACCESS_TOKEN = "TEST"
+
+HEADERS = {
+    "Authorization": f"Bearer {ACCESS_TOKEN}"
+}
 
 st.set_page_config(
     page_title="AI Trading Assistant",
