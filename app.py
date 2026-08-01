aab3e5e6aa141d79117cc96cc97f1155abf4c424
sambage29-max <sambage29@gmail.com>
2026-08-02 00:32:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -114,20 +114,6 @@ with col1:
 with col2:
     st.metric("MACD", "Bullish 🟢")
 
-with col3:
-    st.metric("Accuracy", "75%")
-st.subheader("📊 AI Score")
-
-col1, col2, col3 = st.columns(3)
-
-with col1:
-    st.metric("RSI", "52.4")
-
-with col2:
-    st.metric("MACD", "Bullish 🟢")
-
-with col3:
-    st.metric("Accuracy", "75%")
 # -----------------------
 # Market Summary
 # -----------------------
