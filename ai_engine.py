fc28b11b9b8d95efb7767414c9e7899f997ba19a
sambage29-max <sambage29@gmail.com>
2026-08-05 15:44:58 +0530

    Create ai_engine.py

---
+++ ai_engine.py
@@ -0,0 +1,19 @@
+def ai_decision(rsi, ema20, ema50, macd, adx):
+    """
+    AI Brain
+    Input:
+        RSI
+        EMA20
+        EMA50
+        MACD
+        ADX
+
+    Output:
+        Signal
+        Score
+        Confidence
+    """
+
+    score = 0
+
+    return score
