4a01ea78104a2705ee289276b99648abebf81ba2
sambage29-max <sambage29@gmail.com>
2026-08-05 15:53:50 +0530

    Update ai_engine.py

--- ai_engine.py
+++ ai_engine.py
@@ -17,9 +17,49 @@ def ai_decision(rsi, ema20, ema50, macd, adx):
     score = 0
 # RSI Analysis
 
+# MACD Analysis
+
+# EMA Analysis
+
+# ADX Analysis
+
+if adx >= 25:
+    score += 25
+
+else:
+    score -= 10
+
+if ema20 > ema50:
+    score += 25
+
+else:
+    score -= 25
+if macd == "Bullish":
+    score += 25
+
+else:
+    score -= 25
 if rsi >= 60:
     score += 25
 
 elif rsi <= 40:
     score -= 25
-    return score
+
+  # Final Decision
+
+if score >= 75:
+    signal = "STRONG BUY"
+
+elif score >= 25:
+    signal = "BUY"
+
+elif score <= -75:
+    signal = "STRONG SELL"
+
+elif score <= -25:
+    signal = "SELL"
+
+else:
+    signal = "WAIT"
+
+return signal, score
